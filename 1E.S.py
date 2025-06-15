39abbdd0ca98562549ac02f167046f9ae8f51764
KevinPCE <kevinpcdcmps3555@gmail.com>
2025-06-15 12:04:44 -0600

    agregué los archivos 1E.S.py, E.S.py, Trabajogrupal5.py, holamundo.py

---
+++ 1E.S.py
@@ -0,0 +1,20 @@
+#nombre = str(input("¿Cómo se llama? "))
+#print("Hola" , nombre)
+
+def determinarAltura(pAltura):
+    resultado =""
+    if pAltura1.70:
+            resultado = "Es bajo"
+    else:
+         if pAltura < 1.80 
+              resultado = "Es mediano"
+        else:
+if pAltura <= 1.90:
+                resultado = "Es alto"
+else:
+                resultado = "Es muy alto"   
+return resultado    
+
+    altura = float(input("¿Cuál es su altura? ") )
+    print("le confirmo que su altura es:", altura)
+    print(determinarAltura(altura)) +altura = 1.7
---
+++ E.S.py
@@ -0,0 +1,15 @@
+nombre = str(input("¿Cómo se llama? "))
+edad = str(input("¿Edad? "))
+añodeingreso = str(input("¿año de ingreso a la universidad? "))
+añoscarrera = str(input("¿Cantidad de años de la carrera? "))
+añograduación = str(input("¿año que se gradurá? "))
+edadfinalcarrera = str(input("¿Edad que tendrá cuando termine la carrera? "))
+   
+print:("El nombre es:" +nombre)
+print:("La edad es:" +edad)
+print:("El año de ingreso a la universidad es:" +añodeingreso)
+print:("Los años cursando la carrera son:" +añoscarrera)
+print:("El año en que se gradurá es:" +añograduación)
+print:("La edad al fnal de carrera es:" +edadfinalcarrera)
+
+
