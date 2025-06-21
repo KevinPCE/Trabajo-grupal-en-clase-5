5f8e083ae4903aba4ab021814cc3f66100e63113
KevinPCE <kevinpcdcmps3555@gmail.com>
2025-06-20 21:09:44 -0600

    agregué el quiz 2

---
+++ quiz_2_kevin_picado_campos.py
@@ -0,0 +1,21 @@
+def convertirtemperatura():
+    temperatura=0
+opcion1 = input("ingrese 1 si quiere ver de Farenheit a celsius: ",)
+print("opción de farenheit a celsius", opcion1)
+opcion2 = input("ingrese 2 si quiere ver de celsius a Farenheit: ",)
+
+
+temperatura1 = float (input("ingrese la primer temperatura"))
+print("primer temperatura", temperatura1)
+temperatura2 = float(input("ingrese la segunda temperatura"))
+print("primer temperatura", temperatura2)
+temperatura3 = float (input("ingrese la tercer temperatura"))
+print("primer temperatura", temperatura3)
+
+
+opcion1 = (temperatura1*(5/9))*-32
+opcion2 = (temperatura1*(5/9))*+32
+opcion1 = (temperatura2*(5/9))*-32
+opcion2 = (temperatura2*(5/9))*+32
+opcion1 = (temperatura3*(5/9))*-32
+opcion2 = (temperatura3*(5/9))*+32
