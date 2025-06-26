58e8fadc0a36ce59fd1d1d37ee877205a41503ee
KevinPCE <kevinpcdcmps3555@gmail.com>
2025-06-25 18:29:05 -0600

    agregué el trabajo grupal 6

--- Trabajo_grupal_6_contar 0a100.py
+++ Trabajo_grupal_6_contar 0a100.py
@@ -1,7 +1,16 @@
-def main():
-    # Contar del 0 al 100
-    for numero in range(101):
-     sumatotal += numero
-    print(f"El número es: {numero} y la suma total es: {sumatotal}")
+# Función que calcula la suma del 0 al 100
+def calcular_suma():
+    suma = 0
+    for i in range(101):  # Incluye el 100
+        suma += i
+    return suma
 
+# Función principal
+def main():
+    resultado = calcular_suma()
+    print(f"La suma de los números del 0 al 100 es: {resultado}")
+
+# Llamada a main
+if __name__ == "__main__":
+    main()
 
