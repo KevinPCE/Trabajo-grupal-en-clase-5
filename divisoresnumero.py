77fc5933d7b63101c04bf28573cfd05f6dc08601
KevinPCE <kevinpcdcmps3555@gmail.com>
2025-07-02 21:55:44 -0600

    agregue divisores de un número

---
+++ divisoresnumero.py
@@ -0,0 +1,11 @@
+n=int(input("Ingrese un número entero positivo: "))
+def divisores(numero):
+      divisores_lista = []
+      for i in range(1, numero + 1):
+        if numero % i == 0:
+            divisores_lista.append(i)
+      return divisores_lista
+
+# Ejemplo de uso:
+numero = n
+print(f"Los divisores de {numero} son: {divisores(numero)}")
